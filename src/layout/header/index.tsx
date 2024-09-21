45519202cbb1715b08733a9de0a4e94a23d78281
Lucas Magaldi <lucas.magaldi@hotmail.com>
2024-09-21 14:14:28 -0300

    refac: header components

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -1,19 +1,32 @@
 import { IoIosNotificationsOutline } from "react-icons/io"
-
+import { MdOutlineVideoLibrary } from "react-icons/md"
+import { BsSearch } from "react-icons/bs"
+import { MdOutlineDownloading } from "react-icons/md"
 
 import spotifyLogo from '../../assets/spotify-icon.svg'
 
 export function Header() {
     return (
-        <header className="flex items-center justify-between">
-            <img src={spotifyLogo} alt="" />
+        <header className="flex items-center justify-between h-11 px-4 py-10">
+            <img src={spotifyLogo} alt="spofity logo" className="h-10 w-10 cursor-pointer"/>
 
-            <input type="text" placeholder="What do you want to play"/>
+            <div className="flex items-center min-w-[560px] bg-[#1f1f1f] px-4 py-3 rounded-3xl">
+                <button className="mr-3">
+                    <BsSearch size={24}/>
+                </button>          
+                <input type="text" placeholder="What do you want to play?" className="w-[500px] bg-transparent"/>
+                <button className="border-l-2 pl-3">
+                    <MdOutlineVideoLibrary size={24}/>
+                </button>
+            </div>
 
-            <div className="flex items-center">
-                <span>Download app</span>
-                <IoIosNotificationsOutline />
-
+            <div className="flex items-center gap-10">
+                <button className="flex items-center gap-1 font-semibold">
+                    <MdOutlineDownloading size={22}/>
+                    <span>Download app</span>
+                </button>
+                
+                <IoIosNotificationsOutline size={22}/>
                 <div>
                     <img src="" alt="" />
                 </div>
